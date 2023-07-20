289a88dabcf502f81e4aac5e2591c70265843e98
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-07-20 16:06:50 -0400

    updates and cleans up put request. optimizes it and checks all parameters

--- server/routes/api/UserRoutes.js
+++ server/routes/api/UserRoutes.js
@@ -17,7 +17,6 @@ router.get("/get/:id", auth, async (req, res) => {
 		}
 
 		const user = await User.findById(req.params.id);
-		console.log(req.params.id);
 
 		if (!user) {
 			return res.status(404).send("User not found.");
@@ -106,9 +105,8 @@ router.post("/post/login", async (req, res) => {
 // **************************
 
 // Update User name, email, or password, usable route for all three separate update functions.
-// Would require the update requests to be sent indivdually, not designed for multiple updates at once.
-// /api/user/put/:id
-router.put("/", auth, async (req, res) => {
+// /api/user/put/
+router.put("/put/", auth, async (req, res) => {
 	try {
 		const {
 			newFirstName,
@@ -118,61 +116,83 @@ router.put("/", auth, async (req, res) => {
 			confirmationNewPassword,
 			existingPassword,
 		} = req.body;
-		let updatedField = {};
 
-		const userId = req.user._id;
-
-		if (newFirstName) {
-			updatedField.first_name = newFirstName;
-		}
-
-		if (newLastName) {
-			updatedField.last_name = newLastName;
-		}
-
-		if (newEmail) {
-			updatedField.email = newEmail;
-		}
-
-		if (newPassword) {
-			// if resetting password, asks for existing password and new password typed twice
-			if (!existingPassword) {
-				return res.status(400).send("Please enter your existing password.");
-			}
-
-			const user = await User.findById(userId);
-			// confirmation that existing password is correct
-			const verified = await user.isCorrectPassword(existingPassword);
-
-			if (!verified) {
-				return res
-					.status(400)
-					.send("Your existing password is incorrect. Please try again.");
-			}
-
-			// confirmation that new password typed twice matches
-			if (newPassword !== confirmationNewPassword) {
-				return res.status(400).send("New passwords don't match.");
-			}
-
-			updatedField.password = newPassword;
-		}
-
-		if (Object.keys(updatedField).length === 0) {
+		// Checks to make sure at least one field is being updated
+		// add a check that at least one designated field is being updated
+		if (!newFirstName && !newLastName && !newEmail && !newPassword) {
 			return res.status(400).send("No updates provided.");
 		}
 
-		const updatedUser = await User.findByIdAndUpdate(userId, updatedField, {
-			new: true,
-		});
+		// Grabs user ID from auth req.user.data
+		const userId = req.user.data._id;
 
-		if (!updatedUser) {
+		// Finds user by ID to check other user values and ensure user exists
+		let userQuery = User.findById(userId).select("-password");
+		if (newPassword) {
+			userQuery = User.findById(userId);
+		}
+
+		const user = await userQuery;
+		let changes = 0;
+
+		if (!user) {
 			return res.status(404).send("User not found.");
 		}
 
-		res
-			.status(200)
-			.json({ message: `Account for ${updatedUser.first_name} updated!`, updatedUser });
+		// Checks to see if new first name, last name, or email is provided and if it is different from existing info
+		if (newFirstName && newFirstName !== user.first_name) {
+			user.first_name = newFirstName;
+			changes++;
+		}
+
+		if (newLastName && newLastName !== user.last_name) {
+			user.last_name = newLastName;
+			changes++;
+		}
+
+		if (newEmail && newEmail !== user.email) {
+			user.email = newEmail;
+			changes++;
+		}
+
+		// Checks to see if new password is provided, with appropriate confirmation checks
+		if (newPassword) {
+			// Checks to allow password rest and add new password to user
+			if (!confirmationNewPassword) {
+				return res.status(400).send("Please confirm your new password.");
+			} else if (newPassword !== confirmationNewPassword) {
+				// confirmation that new password typed twice matches
+				return res.status(400).send("New passwords don't match.");
+			} else if (!existingPassword) {
+				// if resetting password, asks for existing password and new password typed twice
+				return res.status(400).send("Please enter your existing password.");
+			} else {
+				const verified = await user.isCorrectPassword(existingPassword);
+
+				if (!verified) {
+					return res
+						.status(400)
+						.send("Your existing password is incorrect. Please try again.");
+				} else {
+					user.password = newPassword;
+					changes++;
+				}
+			}
+		}
+
+		if (changes === 0) {
+			return res.status(400).send("All info provided same as existing info.");
+		}
+
+		console.log("hello", user);
+
+		// Finally, update user info with new info
+		const updatedUser = await user.save();
+
+		res.status(200).json({
+			message: `Account for ${updatedUser.first_name} ${updatedUser.last_name} updated!`,
+			updatedUser,
+		});
 	} catch (err) {
 		return res.status(400).json(err);
 	}
@@ -190,7 +210,6 @@ router.delete("/delete", auth, async (req, res) => {
 		const { confirmationPassword } = req.body;
 
 		const user = await User.findById(req.user.data._id);
-		console.log(user);
 
 		if (!user) {
 			return res.status(404).send("User not found.");
