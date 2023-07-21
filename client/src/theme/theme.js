2bf5962cde23cb24f61812e6c83c6fba5dfa4b6e
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-07-21 02:16:43 -0400

    adjusts Link component styling

--- client/src/theme/theme.js
+++ client/src/theme/theme.js
@@ -3,14 +3,25 @@ import { extendTheme } from "@chakra-ui/react";
 const theme = extendTheme({
 	fonts: {
 		heading: "Bitter, Sans-Serif",
-		text: "Lato, Sans-Serif",
-		button: "Lato, Sans-Serif",
+		body: "Lato, Sans-Serif",
+	},
+	// make links not have text decoration and a different hover color
+	components: {
+		Link: {
+			baseStyle: {
+				_hover: {
+					textDecoration: "none",
+					color: "brand.400",
+				},
+			},
+		},
 	},
 	colors: {
 		brand: {
 			100: "#000000",
 			200: "#FFFFFF",
 			300: "#0D968B",
+			400: "#2F4858",
 		},
 	},
 });
