bc2a3848fbcf1d9a32d665b09baba36428fa9ef5
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-07-21 01:44:29 -0400

    finishes beginning styling of homepage, adds links to buttons

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Box, Heading, Text, Button } from "@chakra-ui/react";
 import Header from "../components/Header.jsx";
 import BrandButton from "../components/BrandButton.jsx";
+import { Link } from "react-router-dom";
 
 const Home = () => {
 	return (
@@ -15,16 +16,22 @@ const Home = () => {
 				alignItems="center"
 			>
 				<Header />
-				<Heading my={5}>Welcome to Chattitude!</Heading>
-				<Text fontSize="xl" maxW="50%" textAlign="center" p={5}>
-					<span style={{ color: "#0D968B", fontWeight: "bold" }}>Chattitude</span> is a
-					chat app designed to host unfiltered conversations between any and all people
-					who want to talk about, well, whatever! Sign up or log in, and let's get
-					chatting!
-				</Text>
-				<Box display="flex" flexDirection="row" gap={8}>
-					<BrandButton width="100%">Log In</BrandButton>
-					<BrandButton width="100%">Sign Up</BrandButton>
+				<Box display="flex" flexDirection="column" alignItems="center" my={20} gap={6}>
+					<Heading my={5}>Welcome to Chattitude!</Heading>
+					<Text fontSize="xl" maxW="50%" textAlign="center" p={5}>
+						<span style={{ color: "#0D968B", fontWeight: "bold" }}>Chattitude</span> is a
+						chat app designed to host unfiltered conversations between any and all people
+						who want to talk about, well, whatever! Sign up or log in, and let's get
+						chatting!
+					</Text>
+					<Box display="flex" flexDirection="row" gap={8}>
+						<Link to="/login">
+							<BrandButton width="100%">Log In</BrandButton>
+						</Link>
+						<Link to="/signup">
+							<BrandButton width="100%">Sign Up</BrandButton>
+						</Link>
+					</Box>
 				</Box>
 			</Box>
 		</>
