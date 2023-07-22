8ab76956577b2018580fc48c1a6377eda43bff32
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-07-22 09:59:42 -0400

    fixes async issues on login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import { Box, Heading, FormControl, FormLabel, Input, Text } from "@chakra-ui/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BrandButton from "../components/BrandButton.jsx";
 import Header from "../components/Header.jsx";
 import { set } from "mongoose";
@@ -9,55 +9,68 @@ function Login() {
 	const [passwordInput, setPasswordInput] = useState("");
 	const [emailError, setEmailError] = useState(false);
 	const [passwordError, setPasswordError] = useState(false);
+	const [submitted, setSubmitted] = useState(false);
+	const [makeFetch, setMakeFetch] = useState(false);
 	let emailMessage = emailError ? "Email is required" : "";
 	let passwordMessage = passwordError ? "Password is required" : "";
 
 	const signIn = async () => {
 		setEmailError(false);
 		setPasswordError(false);
-
-		if (emailInput.trim() === "") {
-			setEmailError(true);
-		}
-		if (passwordInput.trim() === "") {
-			setPasswordError(true);
-		}
-
+		setSubmitted(true);
 		console.log("sign in attempt");
 		console.log("Email: " + emailInput);
 		console.log("Password: " + passwordInput);
+	};
 
-		if (!emailError && !passwordError) {
-			try {
-				console.log("log in attempt");
-				console.log("Email: " + emailInput);
-				console.log("Password: " + passwordInput);
+	async function fetchData() {
+		try {
+			const response = await fetch("/api/user/post/login", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					email: emailInput,
+					password: passwordInput,
+				}),
+			});
+			if (response.ok) {
+				const data = await response.json();
+				// store token in cookies
+				document.cookie = `token=${data.token}`;
+				// redirect to messages
+				window.location.href = "/websockettest";
+			} else {
+				const errorData = await response.json();
+				alert(errorData.error);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	}
 
-				const response = await fetch("/api/user/post/login", {
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json",
-					},
-					body: JSON.stringify({
-						email: emailInput,
-						password: passwordInput,
-					}),
-				});
-				if (response.ok) {
-					const data = await response.json();
-					// store token in cookies
-					document.cookie = `token=${data.token}`;
-					// redirect to messages
-					window.location.href = "/websockettest";
-				} else {
-					const errorData = await response.json();
-					alert(errorData.error);
-				}
-			} catch (error) {
-				console.log(error);
+	useEffect(() => {
+		if (submitted) {
+			if (emailInput.trim() === "") {
+				setEmailError(true);
+			}
+			if (passwordInput.trim() === "") {
+				setPasswordError(true);
+			}
+			if (emailInput.trim() !== "" && passwordInput.trim() !== "") {
+				setMakeFetch(true);
 			}
 		}
-	};
+	}, [submitted && !emailError && !passwordError]);
+
+	useEffect(() => {
+		if (makeFetch) {
+			fetchData();
+			setSubmitted(false);
+			setMakeFetch(false);
+		}
+	}, [makeFetch]);
 
 	const updateEmail = (event) => {
 		setEmailInput(event.currentTarget.value);
@@ -82,10 +95,12 @@ function Login() {
 				<FormControl bgColor="white" w="50%" p={5} m={5} borderRadius={5}>
 					<FormLabel>Email</FormLabel>
 					<Input
+						type="email"
 						placeholder="Email"
 						onChange={updateEmail}
 						onBlur={updateEmail}
 						isInvalid={emailError}
+						id="emailInput"
 					/>
 					<Text pb={3} color="red.600">
 						{emailMessage}
@@ -97,6 +112,7 @@ function Login() {
 						onChange={updatePassword}
 						onBlur={updatePassword}
 						isInvalid={passwordError}
+						id="passwordInput"
 					/>
 					<Text pb={3} color="red.600">
 						{passwordMessage}
