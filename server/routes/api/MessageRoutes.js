308895d44ae73c768a842e02207f16a72f8b22d3
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-11-09 18:18:34 -0500

    test message save error

--- server/routes/api/MessageRoutes.js
+++ server/routes/api/MessageRoutes.js
@@ -72,6 +72,8 @@ router.post("/create", async (req, res) => {
 			first_name,
 		});
 
+		console.log(messageData);
+
 		res.status(200).json(messageData);
 	} catch (err) {
 		res.status(400).json(err);
