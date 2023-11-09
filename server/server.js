26b0eb4ae389a2d606a959d3697cea7fd660c059
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-11-09 17:30:27 -0500

    fix dist

--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ wsServer.on("connection", function (connection) {
 // Express middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "..", "client", "public")));
+app.use(express.static(path.join(__dirname, "..", "client", "dist")));
 
 app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname, "..", "client", "dist", "index.html"));
