efbed08f6575b71ff617dd993a77895167fcaaea
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-11-09 17:53:41 -0500

    fix

--- server/server.js
+++ server/server.js
@@ -41,6 +41,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "..", "client", "dist")));
 
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "..", "client", "dist", "index.html"));
+});
+
 // Routes
 app.use(routes);
 
